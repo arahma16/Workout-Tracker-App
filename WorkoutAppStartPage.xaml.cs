581921338c2d0d0338b436da6dab672065568c15
arahma16 <39509980+arahma16@users.noreply.github.com>
2020-12-16 17:35:59 -0800

    Initial Reupload

---
+++ WorkoutAppStartPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorkoutTrackerApp
+{
+    /// <summary>
+    /// Interaction logic for WorkoutAppStartPage.xaml
+    /// </summary>
+    public partial class WorkoutAppStartPage : Page
+    {
+        public WorkoutAppStartPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void WorkoutClick(object sender, RoutedEventArgs e)
+        {
+            // Go to Workout Page
+            workoutPage wPage = new workoutPage();
+            this.NavigationService.Navigate(wPage);
+        }
+
+        private void DietClick(object sender, RoutedEventArgs e)
+        {
+            // Go to Workout Page
+            workoutPage wPage = new workoutPage();
+            this.NavigationService.Navigate(wPage);
+        }
+
+        private void ProgressClick(object sender, RoutedEventArgs e)
+        {
+            // Go to Workout Page
+            workoutPage wPage = new workoutPage();
+            this.NavigationService.Navigate(wPage);
+        }
+
+        private void HowToClick(object sender, RoutedEventArgs e)
+        {
+            // Go to Workout Page
+            workoutPage wPage = new workoutPage();
+            this.NavigationService.Navigate(wPage);
+        }
+    }
+}
---
+++ workoutPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorkoutTrackerApp
+{
+    /// <summary>
+    /// Interaction logic for workoutPage.xaml
+    /// </summary>
+    public partial class workoutPage : Page
+    {
+        public workoutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeClick(object sender, RoutedEventArgs e)
+        {
+            // Go to Workout Page
+            WorkingOutPage nPage = new WorkingOutPage();
+            this.NavigationService.Navigate(nPage);
+        }
+
+        private void WorkoutClick(object sender, RoutedEventArgs e)
+        {
+            // Go to Workout Page
+            WorkingOutPage nPage = new WorkingOutPage();
+            this.NavigationService.Navigate(nPage);
+        }
+
+        private void CardioClick(object sender, RoutedEventArgs e)
+        {
+            // Go to Workout Page
+            WorkingOutPage nPage = new WorkingOutPage();
+            this.NavigationService.Navigate(nPage);
+        }
+    }
+}
